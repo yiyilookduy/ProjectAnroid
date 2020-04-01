91c0eaac591b16148453ce0c99b412deb218b007
DogeMuchWow <cybermaus98@gmail.com>
2020-04-01 12:02:43 +0700

    Update Student Activity

--- app/src/main/java/com/example/groupproject/studentActivity.java
+++ app/src/main/java/com/example/groupproject/studentActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.gson.Gson;
-
 public class studentActivity extends AppCompatActivity {
 
     @Override
