cc710d4cd4432a008576a447f3fb53894c8bc362
DogeMuchWow <cybermaus98@gmail.com>
2020-04-12 14:17:08 +0700

    Remove utils package

--- app/src/main/java/utils/JSONManipulator.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class JSONManipulator {
-    private String usernameJsonData = "";
-    private String passwordJsonData = "";
-    private int roleIdJsonData = 0;
-    private Boolean activeJsonData = false;
-
-}
